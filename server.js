45bbc50cf3f0931d72d99a9d1667185a82e90764
sbrycebarker <sbrycesti@gmail.com>
2018-02-19 22:29:13 -0700

    auth0

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ const express = require('express'),
       config = require('./config.js');
       MongoStore = require('connect-mongo')(session);
 
+      var mon = mongoose.connect('mongodb://sbrycebarker:serg1234@ds125628.mlab.com:25628/barkerperformance', {
+        useMongoClient: true,
+      });
+      // console.log(mon)
+
       var app = express();
 
       app.use(bodyParser.json())
@@ -18,7 +23,7 @@ const express = require('express'),
       mongoose.connect('mongodb://sbrycebarker:serg1234@ds125628.mlab.com:25628/barkerperformance', {
         useMongoClient: true,
       })
-
+      
       // app.use(stripe({
       //   stripe.customers.create({
       //     email:
@@ -35,6 +40,7 @@ const express = require('express'),
                   maxAge: null
                 }
       }))
+      app.use(passport.initialize());
 
       if (app.get('env') === 'production') {
         app.set('trust proxy', 1) // trust first proxy
@@ -42,72 +48,55 @@ const express = require('express'),
       }
 
 //<<====================================login==============================>>
+  passport.use(new Auth0Strategy({
+    domain:       config.auth0.domain,
+    clientID:     config.auth0.clientID,
+    clientSecret: config.auth0.clientSecret,
+    callbackURL:  '/callback'
+    },
+    function(accessToken, refreshToken, extraParams, profile, done) {
 
-      passport.use(new Auth0Strategy({
-       domain:       config.auth0.domain,
-       clientID:     config.auth0.clientID,
-       clientSecret: config.auth0.clientSecret,
-       callbackURL:  '/callback'
-      },
-      function(accessToken, refreshToken, extraParams, profile, done) {
-        db.getUserByAuthId([profile.id]).then(function(user) {
-          // console.log('gettinguser', profile)
-          if (!user[0]) {
-             //if there isn't one, we'll create one!
-            // console.log("creating user", profile)
-            db.createUserByAuth([profile.displayName, profile.id]).then(function(user2) {
-              // console.log('USER CREATED', user2);
-              return done(null, profile);
-            })
-          } else {
-            // console.log("found User", profile)
-            // user = user[0]
-            return done(null , user);
-          }
-        })
+      return done(null, profile);
+    }
+  ));
+
+
+  app.get('/callback',
+    passport.authenticate('auth0', { failureRedirect: '/login' }),
+    function(req, res) {
+      if (!req.user) {
+        throw new Error('user null');
       }
-      ));
-
-      passport.serializeUser(function(userA, done) {
-    console.log('serializing', userA);
-    var userB = userA;
-    done(null, userB); //PUTS 'USER' ON THE SESSION
-    });
-
-    //USER COMES FROM SESSION - THIS IS INVOKED FOR EVERY ENDPOINT
-    passport.deserializeUser(function(userB, done) {
-    var userC = userB;
-    //Things you might do here :
-    // Query the database with the user id, get other information to put on req.user
-    done(null, userC); //PUTS 'USER' ON REQ.USER
-    });
-
-    app.get('/auth', passport.authenticate('auth0'));
-
-    app.get('/callback',
-      passport.authenticate('auth0', { failureRedirect: '/login' }),
-      function(req, res) {
-        if (!req.user) {
-          throw new Error('user null');
-        }
-        res.redirect("/");
-      }
-    );
-    app.get('/login',
-      passport.authenticate('auth0', {connection: 'google-oauth2'}), function (req, res) {
       res.redirect("/");
-    });
+    }
+  );
 
-    app.get('/auth/me', function(req, res) {
-      if (!req.user) return res.sendStatus(404);
-      console.log("me", req.user)
-      res.status(200).send(req.user);
-    })
+  app.get('/login',
+    passport.authenticate('auth0', {connection: 'google-oauth2'}), function (req, res) {
+    res.redirect("/");
+  });
 
-    app.get('/auth/logout', function(req, res) {
-      req.logout();
-      res.redirect('/');
-    })
+  app.get('/auth', passport.authenticate('auth0'));
+
+  passport.serializeUser(function(userA, done) {
+  console.log('serializing', userA);
+  var userB = userA;
+  done(null, userB); //PUTS 'USER' ON THE SESSION
+  });
+
+  //USER COMES FROM SESSION - THIS IS INVOKED FOR EVERY ENDPOINT
+  passport.deserializeUser(function(userB, done) {
+  var userC = userB;
+  //Things you might do here :
+  // Query the database with the user id, get other information to put on req.user
+  done(null, userC); //PUTS 'USER' ON REQ.USER
+  });
+
+  app.get('/auth/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+  })
+
 //<<====================================login==============================>>
 
       let users = require('./server/usercrud.js');
