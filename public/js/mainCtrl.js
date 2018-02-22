4f6215c24932d37e6e0cd70b010cdb28c002216b
sbrycebarker <sbrycesti@gmail.com>
2018-02-22 01:21:01 -0700

    login

--- public/js/mainCtrl.js
+++ public/js/mainCtrl.js
@@ -97,26 +97,10 @@ angular.module('app').controller('mainCtrl', function($scope, service, $state) {
   $scope.getUsers();
 
 
-  $scope.createUser = function(data) {
-    let user = data;
-  }
+  // $scope.createUser = function(data) {
+  //   let user = data;
+  // }
 
-  $scope.getUser = function() {
-    service.getUser().then(function(user) {
-      console.log("user", user)
-      if (user) {
-        $scope.user = user[0].username;
-        $scope.userid = user[0].user_id
-        console.log("userinfo", $scope.user)
-        $scope.getfaveBands();
-        $scope.getfaveVenues()
-      } else {
-        $scope.user = 'LOG IN!';
-      }
-    })
-  }
-
-    $scope.getUser();
 
     $scope.logout = service.logout;
 
---
+++ public/js/navCtrl.js
@@ -0,0 +1,20 @@
+angular.module('app').controller('navCtrl', function($scope, service, $state) {
+
+  $scope.getUser = function() {
+    service.getUser().then(function(user) {
+      console.log("user", user)
+      if (user) {
+        $scope.user = user[0].username;
+        $scope.userid = user[0].user_id
+        console.log("userinfo", $scope.user)
+        $scope.getfaveBands();
+        $scope.getfaveVenues()
+      } else {
+        $scope.user = 'LOG IN!';
+      }
+    })
+  }
+
+    $scope.getUser();
+
+})
